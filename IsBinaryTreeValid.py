fd2b9ad1eed6e45f73962378950f99bac7175cd8
ashevc01 <ashevchuk2001@gmail.com>
2020-12-03 09:13:42 -0800

    complete binary tree problem

--- IsBinaryTreeValid.py
+++ IsBinaryTreeValid.py
@@ -1,20 +1,52 @@
-# Question: Check if binary tree is valid
-#===========Solution============
+# Question: Check that binary tree is complete
+# Setup:
 class Node:
 	def __init__(self, val):
 		self.val = val
 		self.left = None
 		self.right = None
-def isBstValid(node):
-	if node is None:
-		return True
-	isCurrentValid = ((node.left is None) or (node.left.val <= node.val)) and ((node.right is None) or (node.right.val >= node.val))
-	return isCurrentValid and isBstValid(node.left) and isBstValid(node.right)
 
-#==============Tests==============
-head = Node(3)
-head.left  = Node(4)
-head.right = Node(5)
-assert not isBstValid(head)
-head.left = Node(1)
-assert isBstValid(head)
+# Both solutions passed on LeetCode
+# A tree is complete if every node except last is filled
+# And tree is far left as possible
+# Complexity: O(n), where n is the number of nodes
+# Solution essentially a BFS with null checking
+# Both sol'ns are O(n) for space, O(n) for time
+
+# Solution 1, using Python Queue:
+# Supposed to give O(1) time, but in practice list implementation is often faster because
+# list stuff is implemented in C and Queue is implemented in Python
+from queue import Queue
+def isTreeCompleteQueue(head):
+	toVisit = Queue()
+	toVisit.put(head)
+	while not toVisit.empty():
+		node = toVisit.get()
+		if node is None:
+			while not toVisit.empty():
+				if not (toVisit.get() is None):
+					return False
+			return True
+		toVisit.put(node.left)
+		toVisit.put(node.right)
+
+# Solution 2: Cleaner solution using Python list. Faster in practice 
+# (amortized times are equal, but list implemented in C so faster)
+# but queue gives better worst case performance
+def isTreeCompleteList(head):
+	toVisit = [head]
+	while True:
+		node = toVisit.pop(0)
+		if node is None:
+			return not any(toVisit)
+		toVisit.extend([node.left, node.right])
+	
+# Tests
+a = Node(1)
+a.left = Node(2)
+a.right = Node(3)
+a.left.left = Node(4)
+a.left.right = Node(5)
+a.right.right = Node(6)
+assert not isTreeCompleteQueue(a)
+assert not isTreeCompleteList(a)
--- RotateMatrix.py
+++ RotateMatrix.py
@@ -7,7 +7,8 @@ def rotateMatrixNumpy(M):
 # 1) Can matrix be None? Assume yes
 # 2) Is matrix square? Assume no
 # 3) Which direction are we rotating in? Assume clockwise
-def rotateSquareMatrixClockwise(M):
+# For CCW rotation just apply method 3 times!
+def rotateRectMatrixClockwise(M):
 	if M is None:
 		return None
 	R = len(M)
@@ -20,8 +21,22 @@ def rotateSquareMatrixClockwise(M):
 
 # Tests
 M1 = None
-assert rotateSquareMatrixClockwise(M1) is None
+assert rotateRectMatrixClockwise(M1) is None
 M2 = [ [1, 2, 3, 3], [4, 5, 6, 6], [7, 8, 9, 9] ]
 M2R = [ [7, 4, 1], [8, 5, 2], [9, 6, 3], [9, 6, 3] ]
-print(rotateSquareMatrixClockwise(M2))
-assert rotateSquareMatrixClockwise(M2) == M2R
+print(rotateRectMatrixClockwise(M2))
+assert rotateRectMatrixClockwise(M2) == M2R
+
+# Question: Rotate square matrix IN PLACE without allocating new array
+# Solution (passed Leetcode):
+class Solution:
+    def rotate(self, matrix):
+        if matrix is None:
+            return
+        #We have oldMatrix[r][c] = newMatrix[c][R - 1 - r]
+        N = len(matrix)
+        for r in range(N):
+            for c in range(r + 1, N):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        for r in range(N):
+            matrix[r] = matrix[r][::-1]
