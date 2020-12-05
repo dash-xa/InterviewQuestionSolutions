9a4d5b40ac8a9ab7e464865fb7346418bc8a198f
ashevc01 <ashevchuk2001@gmail.com>
2020-12-04 23:59:39 -0800

    valid parens

---
+++ ValidParens.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        counts = []
+        for c in s:
+            if c in "({[":
+                counts.append(c)
+            else:
+                if not counts:
+                    return False
+                popped = counts.pop()
+                case1 = c == ")" and popped != "("
+                case2 = c == "]" and popped != "["
+                case3 = c == "}" and popped != "{"
+                if case1 or case2 or case3:
+                    return False
+        return not bool(counts)
